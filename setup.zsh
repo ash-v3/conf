049b895a259a58a6e6f7babfe87000b6632c8ccd
Ashley Vaughn <Trevor.N.Vaughn@gmail.com>
2022-09-19 20:24:30 -0700

    add to setup

--- setup.zsh
+++ setup.zsh
@@ -1,28 +1,38 @@
-systemctl enable dhcpcd
-systemctl start dhcpcd
+sudo systemctl enable dhcpcd
+sudo systemctl start dhcpcd
 
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup
-rankmirrors -n 6 /etc/pacman.d/mirrorlist-backup > /etc/pacman.d/mirrorlist
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup
+sudo rankmirrors -n 6 /etc/pacman.d/mirrorlist-backup > /etc/pacman.d/mirrorlist
 
-pacman -Syu
-pacman -S sudo openssh git
+sudo pacman -Syu
+sudo pacman -S sudo openssh git
 
-pacman -S base-devel go
-mkdir /tmp/aur
+sudo pacman -S base-devel go
+sudo mkdir /tmp/aur
 cd /tmp/aur
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg
-pacman -U *
+sudo pacman -U *
 
 # install packages
-yay -Syyuu vicious ttf-jetbrains-mono bottom firefox alsa alsa-utils base brave-bin feh firefox kitty neovim visual-studio-code-bin github-cli ranger neofetch gnuplot wttr fish dmenu chromium ag fasd htop colordiff feh ncdu pydf dtrx steam wine-ge-custom discord steam arandr atop dkms etckeeper gimp macchanger reflector unrar zoxide tor nyx calibre
+yay -Syyuu vicious ttf-jetbrains-mono bottom alsa alsa-utils base brave-bin feh kitty neovim visual-studio-code-bin github-cli ranger neofetch gnuplot wttr fish dmenu chromium ag fasd htop colordiff feh ncdu pydf dtrx wine-ge-custom discord steam arandr atop dkms gimp macchanger reflector unrar zoxide tor nyx calibre rustup nftables popsicle
 
-systemctl enable tor.service
+sudo systemctl enable tor.service
 
 # awesome config
-mkdir ~/.config/awesome/
-cp -r /etc/xdg/awesome/rc.lua ~/.config/awesome/
-mkdir ~/.config/awesome/themes
-cp -r /usr/share/awesome/themes/default ~/.config/awesome/themes/default
+sudo mkdir ~/.config/awesome/
+sudo cp -r /etc/xdg/awesome/rc.lua ~/.config/awesome/
+sudo mkdir ~/.config/awesome/themes
+sudo cp -r /usr/share/awesome/themes/default ~/.config/awesome/themes/default
+
+sudo chsh -s /usr/bin/fish
+
+sudo sysctl net.ipv4.tcp_ecn=0
+
+sudo systemctl enable nftables.service
+
+# change /etc/makepkg.conf & /etc/pacman.conf
+sudo nvim /etc/makepkg.conf
+sudo nvim /etc/pacman.conf
