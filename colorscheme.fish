8f0e4ce0ffd4b3022cebe9570ce4fe2313c8de0c
Ashley Vaughn <trevor.n.vaughn@gmail.com>
2022-10-11 21:54:54 -0700

    colorscheme.fish

---
+++ colorscheme.fish
@@ -0,0 +1,21 @@
+set -U fish_color_normal ffffff
+set -U fish_color_command ff00ff -b
+set -U fish_color_quote ff7f00
+set -U fish_color_redirection 4000ff
+# set -U fish_color_end
+set -U fish_color_error ff0000 -b
+set -U fish_color_param ff0080
+set -U fish_color_comment 7f00ff
+# set -U fish_color_match
+# set -U fish_color_search_match
+# set -U fish_color_operator
+# set -U fish_color_escape
+# set -U fish_color_cwd
+# set -U fish_color_autosuggestion
+# set -U fish_color_user
+# set -U fish_color_host
+# set -U fish_pager_color_prefix
+# set -U fish_pager_color_completion
+# set -U fish_pager_color_description
+# set -U fish_pager_color_progress
+# set -U fish_pager_color_secondary
